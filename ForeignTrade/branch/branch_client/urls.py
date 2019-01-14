d72961648bbf26ef3b406d7209e38810a8e300c2
122320430 <122320430@qq.com>
2019-01-14 11:29:05 +0800

    client urls

--- ForeignTrade/branch/branch_client/urls.py
+++ ForeignTrade/branch/branch_client/urls.py
@@ -1,9 +1,9 @@
-# from rest_framework.routers import SimpleRouter
-# from .views import ClientModelViewSet
-#
-#
-#
-# router = SimpleRouter()
-# router.register('branch_client',ClientModelViewSet)
-#
-#
+from rest_framework.routers import SimpleRouter
+from .views import ClientModelViewSet
+
+
+
+router = SimpleRouter()
+router.register('branch_client',ClientModelViewSet)
+
+
