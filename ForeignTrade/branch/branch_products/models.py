65fe5aeaac6417d65c9b511ebf512fca8f6dcd98
122320430 <122320430@qq.com>
2019-01-17 13:30:52 +0800

    add tax_rate model

--- ForeignTrade/branch/branch_products/models.py
+++ ForeignTrade/branch/branch_products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from product.models import Product
+from users.models import Company
 # Create your models here.
 
 
 BranchProduct = Product
+#
+#清关税率表
+class ProductTaxRate(models.Model):
+    company = models.ForeignKey(Company,on_delete=models.PROTECT,verbose_name='分公司')
+    product = models.ForeignKey(Product,on_delete=models.PROTECT,verbose_name='产品')
+    tax_rate = models.FloatField(default=0,verbose_name='税率')
